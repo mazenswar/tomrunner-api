1cee492cdbb277706ec94e9b3e84420f4c70fbae
mazenswar <mazenswar@protonmail.com>
2019-03-27 12:16:12 -0400

    maz changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,28 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     @user = User.create(user_params)
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:user][:id])
+    @score = params[:score].reduce(&:+)
+    @game = Game.create(user: @user, score: @score);
+    @user.games << @game;
     render json: @user
   end
 
   private
 
   def user_params
-    params.permit(:name)
+    params.permit(:name, :score, :userId)
   end
 
 end
